50b740c4cb1f270514f6df37b3dd8d7b669fc24e
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2025-04-16 12:27:11 -0500

    feaT: fixed params

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  env: {
+    APP_NAME: process.env.APP_NAME,
+    // local APIs
+    APP_API_USER_SERVICE_LOCAL: process.env.APP_API_USER_SERVICE_LOCAL,
+    APP_API_PERSON_SERVICE_LOCAL: process.env.APP_API_PERSON_SERVICE_LOCAL,
+    APP_API_CORE_SERVICE_LOCAL: process.env.APP_API_CORE_SERVICE_LOCAL,
+    APP_API_ADMISSION_SERVICE_LOCAL:
+      process.env.APP_API_ADMISSION_SERVICE_LOCAL,
+    APP_API_PROGRAM_SERVICE_LOCAL: process.env.APP_API_PROGRAM_SERVICE_LOCAL,
+    APP_API_FILE_SERVICE_LOCAL: process.env.APP_API_FILE_SERVICE_LOCAL,
+    APP_API_REPORT_SERVICE_LOCAL: process.env.APP_API_REPORT_SERVICE_LOCAL,
+    //deployed APIs
+    APP_API_USER_SERVICE: process.env.APP_API_USER_SERVICE,
+    APP_API_PERSON_SERVICE: process.env.APP_API_PERSON_SERVICE,
+    APP_API_CORE_SERVICE: process.env.APP_API_CORE_SERVICE,
+    APP_API_ADMISSION_SERVICE: process.env.APP_API_ADMISSION_SERVICE,
+    APP_API_PROGRAM_SERVICE: process.env.APP_API_PROGRAM_SERVICE,
+    APP_API_FILE_SERVICE: process.env.APP_API_FILE_SERVICE,
+    APP_API_REPORT_SERVICE: process.env.APP_API_REPORT_SERVICE,
+    // keys to access the APIs
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    APP_TOKEN_USER: process.env.APP_TOKEN_USER,
+    APP_TOKEN_ADMIN: process.env.APP_TOKEN_ADMIN,
+    API_RENIEC: process.env.API_URL_RENIEC,
+    API_KEY_RENIEC: process.env.API_KEY_RENIEC,
+    // keys to access the auth0
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
+  }
+}
 
-export default nextConfig;
+export default nextConfig
