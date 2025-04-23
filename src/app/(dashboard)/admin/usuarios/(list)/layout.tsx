2bfa27c34a8812b9a9ea31ba0cfca72cab871d1a
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2025-04-23 10:09:10 -0500

    feaT: add list users

--- src/app/(dashboard)/admin/usuarios/(list)/layout.tsx
+++ src/app/(dashboard)/admin/usuarios/(list)/layout.tsx
@@ -4,8 +4,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col gap-4">
       <HeaderSection
-        title="Módulos de la aplicación"
-        description="Aquí puedes gestionar los módulos de la aplicación."
+        title="Usuarios de la aplicación"
+        description="Administra y gestiona los usuarios y sus permisos en la aplicación."
         disabledActions
       />
       {children}
---
+++ src/app/(dashboard)/admin/usuarios/(list)/page.tsx
@@ -0,0 +1,9 @@
+import { UsersListPage } from '@/modules/users'
+
+export default function Page() {
+  return (
+    <>
+      <UsersListPage userList={[]} />
+    </>
+  )
+}
